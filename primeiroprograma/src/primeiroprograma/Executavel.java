3bad3a793e3a32399f9a3db36733f1b0dd0ce220
aluno <aluno@192.168.0.130>
2024-11-27 16:39:30 -0300

    Segundo Commit

--- primeiroprograma/src/primeiroprograma/Executavel.java
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -5,7 +5,8 @@ public class Executavel {
 	public static void main(String[] args) {
 		System.out.println("ola mundo");
 		System.out.println("Meu nome é Nayara");
-	
-	}
+	      int inteiro;
+	       double numeroDecimal;
+	}      string texto;
 
-}
+}     //Atribu valores as varIáveis 
