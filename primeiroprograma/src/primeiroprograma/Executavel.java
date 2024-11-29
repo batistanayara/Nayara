8aa64958f089a4e8b52386e73464130b2a23318b
aluno <aluno@192.168.0.130>
2024-11-29 10:39:08 -0300

    2 commit

--- primeiroprograma/src/primeiroprograma/Executavel.java
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -5,8 +5,14 @@ public class Executavel {
 	public static void main(String[] args) {
 		System.out.println("ola mundo");
 		System.out.println("Meu nome é Nayara");
-	      int inteiro;
-	       double numeroDecimal;
-	}      string texto;
-
-}     //Atribu valores as varIáveis 
+		int inteiro;
+		double numeroDecimal;
+		String texto;
+		inteiro = 5;
+		numeroDecimal = 5.65;
+		texto = "Nayara";
+		System.out.println("Nayara:"+texto);
+		 System.out.println("numero inteiro:"+inteiro);  
+		 System.out.println("numero decimal:"+numeroDecimal);
+	}    
+}
